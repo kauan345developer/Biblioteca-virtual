f2aa61d239aadde6be6ed0ee3258e4f186c1b61c
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2023-11-28 22:28:41 -0400

    Add files via upload

--- biblioteca/src/pages/Cadastro.jsx
+++ biblioteca/src/pages/Cadastro.jsx
@@ -1,39 +1,153 @@
-import styles from "./cadastro.module.scss"
-import logo from "../assets/LogoBiblioteca.png"
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import styles from "./cadastro.module.scss";
+import logo from "../assets/LogoBiblioteca.png";
 
-function Cadastro(){
-  return(
+function Cadastro() {
+  const [cadastroData, setCadastroData] = useState({
+    nome: "",
+    email: "",
+    repeatEmail: "",
+    senha: "",
+    repeatSenha: "",
+  });
+
+  const history = useHistory();
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCadastroData({
+      ...cadastroData,
+      [name]: value,
+    });
+  };
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!cadastroData.nome.trim()) {
+      errors.nome = "Nome é obrigatório";
+    }
+
+    if (!cadastroData.email.trim()) {
+      errors.email = "Email é obrigatório";
+    }
+
+    if (!cadastroData.repeatEmail.trim()) {
+      errors.repeatEmail = "Repita o email é obrigatório";
+    } else if (cadastroData.email !== cadastroData.repeatEmail) {
+      errors.repeatEmail = "Os emails não coincidem";
+    }
+
+    if (!cadastroData.senha.trim()) {
+      errors.senha = "Senha é obrigatória";
+    }
+
+    if (!cadastroData.repeatSenha.trim()) {
+      errors.repeatSenha = "Repita a senha é obrigatória";
+    } else if (cadastroData.senha !== cadastroData.repeatSenha) {
+      errors.repeatSenha = "As senhas não coincidem";
+    }
+
+    return errors;
+  };
+
+  const handleCadastro = async () => {
+    const formErrors = validateForm();
+
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/api/users/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cadastroData),
+      });
+
+      if (response.ok) {
+        console.log("Cadastro bem-sucedido!");
+        history.push("/login");
+      } else {
+        console.error("Falha no cadastro. Tente novamente.");
+      }
+    } catch (error) {
+      console.error("Erro ao cadastrar:", error.message);
+    }
+  };
+
+  return (
     <div className={styles.container}>
       <div className={styles.login}>
-      <div>
-        <img src={logo} alt="" />
-      </div>
+        <div>
+          <img src={logo} alt="" />
+        </div>
         <div>
           <div>
-            <label htmlFor="">Nome Completo</label>
-            <input type="text" name="" id="" />
+            <label htmlFor="nome">Nome Completo</label>
+            <input
+              type="text"
+              name="nome"
+              id="nome"
+              value={cadastroData.nome}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <label htmlFor="">Email</label>
-            <input type="text" name="" id="" />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={cadastroData.email}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <label htmlFor="">Repita o Email</label>
-            <input type="text" name="" id="" />
+            <label htmlFor="repeatEmail">Repita o Email</label>
+            <input
+              type="email"
+              name="repeatEmail"
+              id="repeatEmail"
+              value={cadastroData.repeatEmail}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <label htmlFor="">Senha</label>
-            <input type="text" name="" id="" />
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              name="senha"
+              id="senha"
+              value={cadastroData.senha}
+              onChange={handleInputChange}
+            />
           </div>
           <div>
-            <label htmlFor="">Repita a senha</label>
-            <input type="text" name="" id="" />
+            <label htmlFor="repeatSenha">Repita a Senha</label>
+            <input
+              type="password"
+              name="repeatSenha"
+              id="repeatSenha"
+              value={cadastroData.repeatSenha}
+              onChange={handleInputChange}
+            />
           </div>
-        <button>Cadastrar</button>
+          <button onClick={handleCadastro}>Cadastrar</button>
         </div>
+        {errors.nome && <div className={styles.error}>{errors.nome}</div>}
+        {errors.email && <div className={styles.error}>{errors.email}</div>}
+        {errors.repeatEmail && <div className={styles.error}>{errors.repeatEmail}</div>}
+        {errors.senha && <div className={styles.error}>{errors.senha}</div>}
+        {errors.repeatSenha && <div className={styles.error}>{errors.repeatSenha}</div>}
       </div>
     </div>
-  )
+  );
 }
 
-export{Cadastro}
+export { Cadastro };
--- biblioteca/src/pages/Login.jsx
+++ biblioteca/src/pages/Login.jsx
@@ -1,26 +1,87 @@
-import styles from "./login.module.scss"
-import logo from "../assets/LogoBiblioteca.png"
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+import styles from "./login.module.scss";
+import logo from "../assets/LogoBiblioteca.png";
 
 function Login() {
-  return(
+  const [loginData, setLoginData] = useState({
+    email: '',
+    senha: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const [loginError, setLoginError] = useState(null);
+  const history = useHistory();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        console.log('Login bem-sucedido!');
+        history.push('/bookpage');
+      } else {
+        const data = await response.json();
+        setLoginError(data.message);
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error.message);
+      setLoginError('Erro ao fazer login. Tente novamente.');
+    }
+  };
+
+  return (
     <div className={styles.container}>
       <div className={styles.login}>
         <div>
           <img src={logo} alt="" />
         </div>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={loginData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={loginData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          {loginError && <div className={styles.error}>{loginError}</div>}
+          <button type="submit">Login</button>
+        </form>
         <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-          <label htmlFor="">Senha</label>
-          <input type="text" name="password" id="password" />
-          <button>login</button>
-        </div>
-        <div>
-          <span>Não tem login, <a href="">Clique Aqui</a></span>
+        <span>
+            Cadastre-se, <a href="/cadastro">Clique Aqui</a>
+          </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export{Login}
+export { Login };

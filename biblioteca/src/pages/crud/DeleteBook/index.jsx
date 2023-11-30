444ad9ac1520816d2be8ad4f9e9e941ac12e4d29
felipeflamarini <felipeflamarini@hotmail.com>
2023-11-30 17:10:55 -0300

    implementando deleteBook

--- biblioteca/src/pages/crud/DeleteBook/index.jsx
+++ biblioteca/src/pages/crud/DeleteBook/index.jsx
@@ -1,11 +1,27 @@
 import styles from "./styles.module.scss"
 
 function DeleteBook() {
+
+  const deleteBook = async() => {
+    try {
+      const bookId = document.querySelector('#deleteBookId').value;
+      const response = await fetch(`http://localhost:3000/api/books/delete/${bookId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log(data); // data.message = mensagem do livro excluído
+    } catch (error) {
+      console.error('Erro ao deletar o livro:', error);
+    }
+  }
   return(
   <div className={styles.container}>
     <label htmlFor="">ID para excluir</label>
-    <input type="text" />
-    <button>excluir</button>
+    <input type="text" id="deleteBookId" />
+    <button onClick={deleteBook}>excluir</button>
   </div>
   )
 }
